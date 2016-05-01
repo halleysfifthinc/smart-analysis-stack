126c58b1f60335d08c95db58aa1d2418611ed7d7
halleysfifthinc <allenofthehills@gmail.com>
2016-05-01 16:23:16 -0500

    Revolution data sanitization checks and mitigations; optimizations

--- nsi.m
+++ nsi.m
@@ -22,25 +22,28 @@ pks = pks(x);
 locs = locs(x);
 clear x
 standard_length = 360;
-% TODO: Add check for full revolution (in cases where they are wobbling
-% right around TDC, will introduce multiple peaks above 300)
-revs = length(pks)-2;
 
-%Pre-allocation for speed
-index{1,revs} = [];
-final_index{1,revs} = [];
-x{1,revs} = [];
-xLength = zeros(1,revs);
-
+n = 1;
+revs = 0;
+invalid_revs = [];
 %Interpolate each revolution to be a standard length
-for n=1:revs
-  %Get the index range and length of the revolution
-  x{n} = (locs(n)+1):locs(n+1);
-  xLength(n) = length(x{n});
-  tmp_angles_l = circshift(angles_l(x{n}),floor(xLength(n)/2));
-  tmp_angles_r = angles_r(x{n});
-  tmp_l{n} = interp1(handlebar_angles(x{n}),tmp_angles_l,linspace(0,360,standard_length),'linear','extrap');
-  tmp_r{n} = interp1(handlebar_angles(x{n}),tmp_angles_r,linspace(0,360,standard_length),'linear','extrap');
+while n < (length(pks)-2)
+  %Get the index range and length of the revolution if the revolution is
+  %more than 50 frames long (validity check: 50 frames at 100 fps is pretty
+  %fast) and it begins and ends within 2.5% of 0 deg or 360 deg (to ensure 
+  %that a full forward revolution was made, not a reverse revolution, or 
+  %some partial reverse in direction
+  if (locs(n+1)-(locs(n)+1))>50 && handlebar_angles(locs(n)+1)<9 ...
+          && handlebar_angles(locs(n+1))>351
+      revs = revs+1;
+      x{revs} = (locs(n)+1):locs(n+1);
+      xLength(revs) = length(x{revs});
+      tmp_angles_l = circshift(angles_l(x{revs}),floor(xLength(revs)/2));
+      tmp_angles_r = angles_r(x{revs});
+      tmp_l{revs} = interp1(handlebar_angles(x{revs}),tmp_angles_l,linspace(0,360,standard_length),'linear','extrap');
+      tmp_r{revs} = interp1(handlebar_angles(x{revs}),tmp_angles_r,linspace(0,360,standard_length),'linear','extrap');
+  end
+  n = n+1;
 end
 
 %%Perform NSI calculation for each revolution
@@ -48,14 +51,6 @@ end
 %ensures that no partial revolutions due to the beginning/ending of the
 %data are included.
 for n=1:revs
-  %Get the index range and length of the revolution
-%   x{n} = (locs(n)+1):locs(n+1);
-%   xLength(n) = length(x{n});
-  %Copy the data from the left and right input angles for this revolution
-  %and shift the left
-%   tmp_l = circshift(angles_l(x{n}),floor(xLength(n)/2));
-%   tmp_r = angles_r(x{n});
-
   min_r = min(tmp_r{n});
   min_l = min(tmp_l{n});
   max_r = max(tmp_r{n});
@@ -63,35 +58,26 @@ for n=1:revs
 
   range_r = max_r - min_r;
   range_l = max_l - min_l;
-
-  %Calculate the NSI for this revolution
-  for m=1:standard_length
-    norm_r = (tmp_r{n}(m) - min_r)/range_r+1;
-    norm_l = (tmp_l{n}(m) - min_l)/range_l+1;
-    index{n}(m,1) = ((norm_r - norm_l)/((norm_r + norm_l)*.5))*100;
+  
+  % Skip a revolution if the range is zero, will cause a divide by zero
+  % error in the norm_r or norm_l calculations
+  if range_r == 0 || range_l == 0
+    invalid_revs = [invalid_revs, n];
+    nsi_a(n,1:standard_length) = 0;
+    continue
+  else
+    %Calculate the NSI for this revolution
+    norm_r = (tmp_r{n} - min_r)./range_r+1;
+    norm_l = (tmp_l{n} - min_l)./range_l+1;
+    nsi_a(n,:) = ((norm_r - norm_l)./((norm_r + norm_l).*.5)).*100;
   end
 end
 
-% %Interpolate the NSI of each revolution to be a standard length, the length
-% %of the shortest revolution
-% for n=1:length(x)
-%   index{n} = interp1(handlebar_angles(x{n}),index{n},linspace(0,360,standard_length),'linear','extrap');
-% end
-
-%Pre-allocation for speed
-%num = length(x);
-nsiStd = zeros(revs,standard_length);
-nsiMean = zeros(revs,standard_length);
-
-%Get the mean and Std of the interpolated NSI's
-for n=1:revs
-  for m=1:standard_length
-    nsiStd(n,m) = index{n}(m);
-    nsiMean(n,m) = index{n}(m);
-  end
+% Don't use invalid revolutions
+if length(invalid_revs) ~= 0
+  nsi_a(invalid_revs,:) = [];
 end
-
-nsiStd_final = std(abs(nsiStd),1);
-nsiMean_final = mean(nsiMean,1);
+nsiStd_final = std(abs(nsi_a),1);
+nsiMean_final = mean(nsi_a,1);
 
 end
